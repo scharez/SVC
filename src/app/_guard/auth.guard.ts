1e17d93081574aa49b5f16b1d5e43d7c9b51334f
fionafeichtner <fionafeichtner@gmail.com>
2020-01-27 08:57:44 +0100

    .

--- src/app/_guard/auth.guard.ts
+++ src/app/_guard/auth.guard.ts
@@ -1,14 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { Observable } from 'rxjs';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
   jwtHelper: JwtHelperService = new JwtHelperService();
 
