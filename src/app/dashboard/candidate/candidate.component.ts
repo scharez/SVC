35aaf8ded3d66e9e059b30838c28b51df6388a12
fionafeichtner <fionafeichtner@gmail.com>
2020-01-30 08:36:20 +0100

    .

--- src/app/dashboard/candidate/candidate.component.ts
+++ src/app/dashboard/candidate/candidate.component.ts
@@ -113,6 +113,9 @@ export class CandidateComponent implements OnInit {
       alert('Nicht alle Felder ausgefüllt!');
     } else {
 
+      console.log("Firstname here:     "+ this.firstName);
+      console.log("Firstname here:     "+ this.updateCandidate.candidate.firstname);
+
       this.updateCandidate.id = this.id;
       this.updateCandidate.candidate.firstname = this.firstName;
       this.updateCandidate.candidate.lastname = this.lastName;
